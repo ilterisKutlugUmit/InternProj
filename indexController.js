5d7494eb361ef97cb7162f8dfaabb8d3ae8db794
ilterisKutlugUmit <42342851+ilterisKutlugUmit@users.noreply.github.com>
2018-08-28 20:28:39 +0300

    Add files via upload

---
+++ indexController.js
@@ -0,0 +1,49 @@
+﻿var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+
+var indexController = function (nav) {
+
+    var getIndex = function (req, res) {
+
+        var url = 'mongodb://localhost:27017/blogDB';
+
+        mongodb.connect(url, function (err, db) {
+
+            var myCOllection = db.collection('post');
+            collection.find({}).toArray(function (err, results) {
+                res.render('index', {
+                    title: 'Ilteriş Blog',
+                    nav: nav,
+                    postObj: results
+                });
+            });
+        });
+    };
+
+
+
+    var getById = function (req, res) {
+        var id = new objectId(req.params.id);
+        var url = 'mongodb://localhost:27017/blogDB';
+
+        mongodb.connent(url, function (err, db) {
+            var collection = db.collection('post');
+
+            collection.findOne({ _id: id }, function (err, results) {
+                res.render('index', {
+                    title: 'Ilteriş Blog',
+                    nav: nav,
+                    postObj: results
+                });
+            });
+        });
+
+    };
+
+    return {
+        getIndex: getIndex,
+        getById: getById
+    };
+};
+
+module.exports = indexController;
---
+++ indexRouter.js
@@ -0,0 +1,19 @@
+﻿var express = require("express");
+var indexRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+
+
+var router = function (nav) {
+    var indexController = require("../controllers/indexController")(nav);
+
+    indexRouter.route("/").get(indexController.getIndex);
+
+    //ileride post/:id olabilir (post router'ına taşı)
+    indexRouter.route("/:id").get(indexController.getById);
+
+    return indexRouter;
+
+};
+
+module.exports = router;
